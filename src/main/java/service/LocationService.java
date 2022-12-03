0d28cfb53eae88465026763531fa735273918062
Hiro <nguyentrunghieugl15112002@gmail.com>
2022-12-03 09:50:21 +0700

    Add querry access location

---
+++ src/main/java/service/LocationService.java
@@ -0,0 +1,73 @@
+package service;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import dbservice.DatabaseConnector;
+
+public class LocationService {
+	public boolean isExistAccount(String username, String password) {
+		try {
+	        String sql = "Select * from accounts where username=? and password=?";
+	    	PreparedStatement st = DatabaseConnector.connect.prepareStatement(sql);
+	        st.setString(1, username);
+	        st.setString(2, password);
+	        ResultSet result = st.executeQuery();
+	        return result.next();			
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		return false;
+	}
+	public ArrayList<String> getAllProvince(){
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+			String sql = "SELECT name from provinces";
+			PreparedStatement st = DatabaseConnector.connect.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				result.add(res.getString(1));
+			};
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public ArrayList<String> getAllDistrictsOfProvince(String province_name){
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+			String sql = "SELECT districts.name FROM districts JOIN provinces ON districts.province_code = provinces.code WHERE provinces.name=?";
+			PreparedStatement st = DatabaseConnector.connect.prepareStatement(sql);
+			st.setString(1, province_name);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				result.add(res.getString(1));
+			};
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public ArrayList<String> getAllWardsOfDistricts(String district_name,String province_name){
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+			String sql = "SELECT wards.name FROM wards "
+					+ "JOIN districts ON wards.district_code = districts.code"
+					+ "JOIN provinces ON districts.province_code = provinces.code"
+					+ "WHERE provinces.name=? AND districts.name =?";
+			PreparedStatement st = DatabaseConnector.connect.prepareStatement(sql);
+			st.setString(1, province_name);
+			st.setString(2, district_name);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				result.add(res.getString(1));
+			};
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
