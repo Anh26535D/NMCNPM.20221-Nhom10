bdf2e6c6f591d0ad37c77bdd78741e18b2aabe5b
Hiro <nguyentrunghieugl15112002@gmail.com>
2022-12-20 10:29:47 +0700

    Add components InputSugestion

--- src/components/SingleSelection.java
+++ src/components/SingleSelection.java
@@ -1,4 +1,210 @@
 package components;
 
-public class SingleSelection {
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboPopup;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import javax.swing.plaf.basic.ComboPopup;
+import java.awt.*;
+import java.util.Vector;
+
+public class SingleSelection extends JComboBox {
+    /**
+     * Creates a <code>JComboBox</code> that takes its items from an
+     * existing <code>ComboBoxModel</code>.  Since the
+     * <code>ComboBoxModel</code> is provided, a combo box created using
+     * this constructor does not create a default combo box model and
+     * may impact how the insert, remove and add methods behave.
+     *
+     * @param aModel the <code>ComboBoxModel</code> that provides the
+     *               displayed list of items
+     * @see DefaultComboBoxModel
+     */
+    public SingleSelection(ComboBoxModel aModel) {
+        super(aModel);
+        setUI(new BasicComboBoxUI() {
+            @Override
+            protected ComboPopup createPopup() {
+                return new BasicComboPopup(comboBox) {
+
+                    @Override
+                    protected JScrollPane createScroller() {
+                        JScrollPane scroller = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                        scroller.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+                            @Override
+                            protected JButton createDecreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            protected JButton createIncreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            public Dimension getPreferredSize(JComponent c) {
+                                return new Dimension(10, super.getPreferredSize(c).height);
+                            }
+
+                            private JButton createZeroButton() {
+                                return new JButton() {
+                                    @Override
+                                    public Dimension getMinimumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getPreferredSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getMaximumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+                                };
+                            }
+                        });
+                        return scroller;
+                    }
+                };
+            }
+        });
+        setPrototypeDisplayValue("This is Text");
+    }
+
+    /**
+     * Creates a <code>JComboBox</code> that contains the elements
+     * in the specified array.  By default the first item in the array
+     * (and therefore the data model) becomes selected.
+     *
+     * @param items an array of objects to insert into the combo box
+     * @see DefaultComboBoxModel
+     */
+    public SingleSelection(Object[] items) {
+        super(items);
+        setUI(new BasicComboBoxUI() {
+            @Override
+            protected ComboPopup createPopup() {
+                return new BasicComboPopup(comboBox) {
+                    @Override
+                    protected JScrollPane createScroller() {
+                        JScrollPane scroller = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                        scroller.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+                            @Override
+                            protected JButton createDecreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            protected JButton createIncreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            public Dimension getPreferredSize(JComponent c) {
+                                return new Dimension(10, super.getPreferredSize(c).height);
+                            }
+
+                            private JButton createZeroButton() {
+                                return new JButton() {
+                                    @Override
+                                    public Dimension getMinimumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getPreferredSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getMaximumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+                                };
+                            }
+                        });
+                        return scroller;
+                    }
+                };
+            }
+        });
+        setPrototypeDisplayValue("This is Text");
+    }
+
+    /**
+     * Creates a <code>JComboBox</code> that contains the elements
+     * in the specified Vector.  By default the first item in the vector
+     * (and therefore the data model) becomes selected.
+     *
+     * @param items an array of vectors to insert into the combo box
+     * @see DefaultComboBoxModel
+     */
+    public SingleSelection(Vector items) {
+        super(items);
+        setUI(new BasicComboBoxUI() {
+            @Override
+            protected ComboPopup createPopup() {
+                return new BasicComboPopup(comboBox) {
+                    @Override
+                    protected JScrollPane createScroller() {
+                        JScrollPane scroller = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                        scroller.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+                            @Override
+                            protected JButton createDecreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            protected JButton createIncreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            public Dimension getPreferredSize(JComponent c) {
+                                return new Dimension(10, super.getPreferredSize(c).height);
+                            }
+
+                            private JButton createZeroButton() {
+                                return new JButton() {
+                                    @Override
+                                    public Dimension getMinimumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getPreferredSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getMaximumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+                                };
+                            }
+                        });
+                        return scroller;
+                    }
+                };
+            }
+        });
+        setPrototypeDisplayValue("This is Text");
+    }
+
+    public static void  main(String args[]){
+        String test[] = {"Đông Anh","Đông Anh","Đông Anh","Đông Anh","Đông Anh","Đông Anh","Đông Anh","Đông Anh","Đông Anh","Đông Anh",};
+        JFrame testFrame = new JFrame();
+        testFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JPanel testPanel= new JPanel(new FlowLayout());
+        testFrame.setContentPane(testPanel);
+        testFrame.setSize(300,300);
+        SingleSelection asd= new SingleSelection(test);
+        testPanel.add(asd);
+        testFrame.setVisible(true);
+    }
 }
