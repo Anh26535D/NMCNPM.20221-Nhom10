07b2d20eb94238d578db16aff78e9becaea64130
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2023-02-08 15:13:48 +0700

    no

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,8 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            /* UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-             */
+           UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
         }
         LoginFrame loginFrame = new LoginFrame();
